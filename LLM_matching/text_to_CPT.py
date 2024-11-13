ce652cfdb090449064467118702423432e257cc4
Erchi Zhang <erchizhang@brandeis.edu>
2024-11-13 04:33:40 -0500

    make files

---
+++ LLM_matching/text_to_CPT.py
@@ -0,0 +1,63 @@
+import openai  # Replace with the GPT API you're using
+import json
+from .pdf_to_text import read_pdf
+
+# Replace 'your_api_key' with your actual GPT API key
+openai.api_key = "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"
+
+
+def predict_cpt_codes(messages):
+    """
+    This function takes input text from a product deck, sends it to the GPT API,
+    and returns a dictionary of CPT codes and their descriptions.
+    """
+
+    try:
+        # API call to the ChatGPT model
+        response = openai.ChatCompletion.create(
+            model="gpt-4o", messages=messages  # Use "gpt-4o" as LLM
+        )
+
+        # Extract the response content
+        cpt_data = response["choices"][0]["message"]["content"].strip()
+
+        # Attempt to parse the output into a dictionary format
+        # cpt_dict = json.loads(cpt_data)  # Use json.loads for safer parsing
+        return cpt_data
+
+    except json.JSONDecodeError:
+        print("Failed to parse JSON response. Here is the raw output:", cpt_data)
+        return {}
+    except Exception as e:
+        print("An error occurred:", e)
+        return {}
+
+
+def pdf_to_cpt(file: str, company: str) -> str:
+    extracted_texts = read_pdf(file=file, company=company)
+    messages = [
+        {
+            "role": "system",
+            "content": "You are an assistant skilled in extracting relevant healthcare CPT codes.",
+        },
+        {
+            "role": "user",
+            "content": (
+                f"Analyze the following text extracted from a healthcare startup’s pitch deck to identify the most relevant CPT codes based on the services described. Match CPT codes that reflect the types of services, care models, or healthcare management approaches detailed in the text. "
+                f"Rank the top 10 matched CPT codes by relevance to the startup's offerings and output the result in the following JSON format: "
+                f"{{'CPT Code 1': 'Description of CPT Code 1', 'CPT Code 2': 'Description of CPT Code 2', ..., 'CPT Code 10': 'Description of CPT Code 10'}}"
+                f"Text: {extracted_texts}\n\n"
+                f"If available and relevant, prioritize codes that are central to the services highlighted in the text."
+            ),
+        },
+    ]
+    cpt_codes = predict_cpt_codes(messages)
+    return cpt_codes
+
+
+# if __name__ == "__main__":
+#     print(
+#         pdf_to_cpt(
+#             file="../data/April+Health+Introduction+Deck.pdf", company="april health"
+#         )
+#     )
